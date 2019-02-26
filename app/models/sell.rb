509a894e75fc215c194774553f3a8ab96ebec559
Vanildo Prates <vprates@gmail.com>
2019-02-25 21:31:22 -0300

    Configuring relation between models

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -2,6 +2,15 @@ class Sell < ApplicationRecord
   include Fae::BaseModelConcern
   enum status: { finished: 0, canceled: 1 }
 
+  belongs_to :discount
+  belongs_to :client
+  
+  has_many :sell_products
+  has_many :products, through: :sell_products
+
+  has_many :sell_services
+  has_many :services, through: :sell_services
+  
   validates :client, presence: true
 
   def fae_display_field
@@ -11,7 +20,4 @@ class Sell < ApplicationRecord
   def self.for_fae_index
     order(:id)
   end
-
-  belongs_to :discount
-  belongs_to :client
 end
