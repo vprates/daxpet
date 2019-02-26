ecfb07ea78f7033a9e64890bb6628d948a6c7dba
Vanildo Prates <vprates@gmail.com>
2019-02-25 21:00:38 -0300

    Customizing FAE

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,14 @@
 class Product < ApplicationRecord
   include Fae::BaseModelConcern
-  has_fae_image :hero_image
+
+  validates :title, uniqueness: true, presence: true
+  validates :price, presence: true
 
   def fae_display_field
     title
   end
+
+  has_fae_image :hero_image
 
   belongs_to :supplier
 end
