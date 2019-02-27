ece1ba784f39170a80c57cb372d95570f58efcbb
Vanildo Prates <vprates@gmail.com>
2019-02-27 15:18:54 -0300

    Campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -7,8 +7,16 @@ class Campaign < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
+  after_save :schedule_emails
+
   def fae_display_field
     title
   end
 
+  def schedule_emails
+    Client.all.each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaignJob.perform_later client, self.title, self.body
+    end
+  end
 end
